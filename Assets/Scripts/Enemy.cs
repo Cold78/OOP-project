7db1cba3a7e5843dc75c4f091c5256a234679f22
Cold78 <kareemelmenawy@hotmail.com>
2024-07-28 21:17:09 +0300

    Enemy movement refined

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -24,16 +24,26 @@ public class Enemy : MonoBehaviour
         {
             gameObject.SetActive(false);
         }
+
     }
 
     void EnemyMovement()
     {
-        float enemyDirectionX = (gameManager.playerController.gameObject.transform.position.x - transform.position.x);
-        float enemyDirectionZ = (gameManager.playerController.gameObject.transform.position.z - transform.position.z);
-        Vector3 lookDirection = new Vector3(enemyDirectionX, 0, enemyDirectionZ).normalized;
-        //Vector3 lookDirection = (gameManager.playerController.gameObject.transform.position - transform.position).normalized;
-       transform.Translate(lookDirection * speed * Time.deltaTime);
+        transform.LookAt(gameManager.playerController.transform, Vector3.up);
+        //transform.Translate(transform.forward * speed * Time.deltaTime);
+       // transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+       var pos = transform.forward * speed * Time.deltaTime;
+        transform.position += new Vector3(pos.x, 0, pos.z);
+        Debug.LogError(transform.forward);
+        Debug.LogError(Vector3.forward);
+
+
+        // float enemyDirectionX = (gameManager.playerController.gameObject.transform.position.x - transform.position.x);
+        // float enemyDirectionZ = (gameManager.playerController.gameObject.transform.position.z - transform.position.z);
+        // Vector3 lookDirection = new Vector3(enemyDirectionX, 0, enemyDirectionZ).normalized;
+        // //Vector3 lookDirection = (gameManager.playerController.gameObject.transform.position - transform.position).normalized;
+        //transform.Translate(lookDirection * speed * Time.deltaTime);
     }
 
-    
+
 }
